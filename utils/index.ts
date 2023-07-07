2aa3d1daafa1e103c48a74676ec980be7e89f0c5
harrytimbog <harriemannie@gmail.com>
2023-07-07 14:24:17 +0100

    Complete Car Detail Component

--- utils/index.ts
+++ utils/index.ts
@@ -4,7 +4,7 @@ export async function fetchCars() {
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
-  const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
+  const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera";
 
   const response = await fetch(url, {
     headers: headers,
