aaf3db7390fb92f2804083ba8ed89607a0c6a173
harrytimbog <harriemannie@gmail.com>
2023-07-08 16:27:15 +0100

    Car Search Functionality | Filter fetch Cars on serverside

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,18 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     "X-RapidAPI-Key": "384be5c147msh985305e80e7f2bep1752abjsnd3e2a75c9e6f",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
-  const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3";
+  const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`;
 
   const response = await fetch(url, {
     headers: headers,
-  });
+  }); 
 
   const result = await response.json();
 
